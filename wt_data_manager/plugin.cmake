8d85f505cd59904711a01c0896fc7ddc3514789b
hategan <hategan@mcs.anl.gov>
2017-04-02 22:49:42 -0700

    added basic test config

---
+++ wt_data_manager/plugin.cmake
@@ -0,0 +1,20 @@
+get_filename_component(PLUGIN ${CMAKE_CURRENT_LIST_DIR} NAME)
+
+add_eslint_test(${PLUGIN}
+    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client")
+add_puglint_test(${PLUGIN}
+    "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/web_client/templates")
+
+add_python_test(
+  wt_data_manager
+  PLUGIN ${PLUGIN}
+)
+
+add_python_style_test(
+  python_static_analysis_${PLUGIN}
+  "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/server"
+)
+add_python_style_test(
+  python_static_analysis_${PLUGIN}_tests
+  "${PROJECT_SOURCE_DIR}/plugins/${PLUGIN}/plugin_tests"
+)
