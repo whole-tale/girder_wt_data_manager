fe85d12a5721b709a614115442d4787ea5cd5a0c
hategan <hategan@mcs.anl.gov>
2017-04-07 14:25:08 -0700

    bleugh; urlopen() says it returns something file-like, but it's not file like enough to be a context

--- wt_data_manager/server/lib/handlers/http.py
+++ wt_data_manager/server/lib/handlers/http.py
@@ -2,9 +2,24 @@ from six.moves.urllib.request import urlopen
 from .common import FileLikeUrlTransferHandler
 
 
+class HttpStream:
+    def __init__(self, stream):
+        self.stream = stream
+
+    def __enter__(self):
+        return self
+
+    def read(self, sz=-1):
+        return self.stream.read(sz)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        # dear python, do these need to be closed if not all data is read?
+        return True
+
+
 class Http(FileLikeUrlTransferHandler):
     def __init__(self, url, transferId, itemId, psPath):
         FileLikeUrlTransferHandler.__init__(self, url, transferId, itemId, psPath)
 
     def openInputStream(self):
-        return urlopen(self.url)
+        return HttpStream(urlopen(self.url))
