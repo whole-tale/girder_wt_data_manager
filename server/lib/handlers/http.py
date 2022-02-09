b6274a03da23029011c733d0cc258c1d15047cf7
hategan <mhategan@gmail.com>
2022-02-08 22:20:32 -0800

    switched to auth_requests

--- server/lib/handlers/http.py
+++ server/lib/handlers/http.py
@@ -6,32 +6,14 @@ import requests
 from .common import FileLikeUrlTransferHandler
 
 
-# we should probably have a better way of doing this
-SCOPE_MAP = {'pbcconsortium.isrd.isi.edu':
-                 'https://auth.globus.org/scopes/a77ee64a-fb7f-11e5-810e-8c705ad34f60/deriva_all'}
-
-
 class Http(FileLikeUrlTransferHandler):
-    def __init__(self, url, transferId, itemId, psPath, user, transferManager):
-        FileLikeUrlTransferHandler.__init__(self, url, transferId, itemId, psPath, user,
-                                            transferManager)
-        self.extra_headers = {}
-        parsed = urllib.parse.urlparse(url)
-        if parsed.netloc in SCOPE_MAP:
-            scope = SCOPE_MAP[parsed.netloc]
-            if 'otherTokens' in user:
-                for token in user['otherTokens']:
-                    if token['scope'] == scope:
-                        self.extra_headers['Authorization'] = 'Bearer ' + token['access_token']
-
-
     def openInputStream(self):
         parsed = urllib.parse.urlparse(self.url)
         if parsed.path and parsed.path.endswith('.zip') and parsed.query:
             qs = urllib.parse.parse_qs(parsed.query)
             path = qs['path'][0]
             fp = httpio.open(urllib.parse.urlunparse((parsed.scheme, parsed.netloc, parsed.path,
-                                                      '', '', '')), headers=self.extra_headers)
+                                                      '', '', '')), headers=self.headers)
             zf = zipfile.ZipFile(fp)
             try:
                 return zipfile.Path(zf, path).open(mode="rb")
